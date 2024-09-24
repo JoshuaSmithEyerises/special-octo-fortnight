24b5173672657e2f190fccdfeb134ec79ff90312
JoshuaSmithEyerises <147095687+JoshuaSmithEyerises@users.noreply.github.com>
2024-09-24 11:49:36 -0400

    Update App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './components/Home'; // Make sure this path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> {/* You can replace this with other components like <About />, <Projects /> */}
+    </div>
+  );
+}
+
+export default App;
