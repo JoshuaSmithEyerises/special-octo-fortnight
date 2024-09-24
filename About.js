aff5e571dbbc966c236bd11c0720303fb80fa148
JoshuaSmithEyerises <147095687+JoshuaSmithEyerises@users.noreply.github.com>
2024-09-24 11:52:12 -0400

    Update About.js

---
+++ About.js
@@ -0,0 +1,7 @@
+const About = () => (
+  <section>
+    <h2>About Me</h2>
+    <p>I’m a Computer Science student at the College of Charleston, specializing in web design, coding, and data analytics.</p>
+  </section>
+);
+export default About;
