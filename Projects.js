911b83011613edca9e1dfd77690671c1c9a2693a
JoshuaSmithEyerises <147095687+JoshuaSmithEyerises@users.noreply.github.com>
2024-09-24 11:53:23 -0400

    Update Projects.js

---
+++ Projects.js
@@ -0,0 +1,10 @@
+const Projects = () => (
+  <section>
+    <h2>Projects</h2>
+    <ul>
+      <li>WaterWatch - Web App</li>
+      <li>Interactive 3D Learning Game</li>
+    </ul>
+  </section>
+);
+export default Projects;
