dc33850c9c21f630a7e1a7346a4f1c2454ab6db3
JoshuaSmithEyerises <147095687+JoshuaSmithEyerises@users.noreply.github.com>
2024-09-24 11:50:54 -0400

    Update Home.js

---
+++ Home.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <section>
+    <h1>Welcome to My Portfolio!</h1>
+    <p>Hi, I'm Joshua Smith, a Computer Science student and developer.</p>
+  </section>
+);
+export default Home;
