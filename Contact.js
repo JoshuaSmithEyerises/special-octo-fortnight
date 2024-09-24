8b5abb47bf139afbef86af3a7662ef98552f2cb4
JoshuaSmithEyerises <147095687+JoshuaSmithEyerises@users.noreply.github.com>
2024-09-24 11:52:43 -0400

    Update Contact.js

---
+++ Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => (
+  <section>
+    <h2>Contact Me</h2>
+    <ul>
+      <li>Email: smithjb4@g.cofc.edu</li>
+      <li><a href="https://linkedin.com/in/joshua-benjamin-smith">LinkedIn</a></li>
+      <li><a href="https://github.com/joshua-smith">GitHub</a></li>
+    </ul>
+  </section>
+);
+export default Contact;
